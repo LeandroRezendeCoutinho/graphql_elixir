1a82f9e67fd1e9803b77e7a7160058fde6d6001a
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-02-04 07:00:51 -0300

    add graphql deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,10 @@ defmodule GraphqlElixir.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.6.1"},
+      {:absinthe_plug, "~> 1.5.4"},
+      {:poison, "~> 2.1.0"}
     ]
   end
 
